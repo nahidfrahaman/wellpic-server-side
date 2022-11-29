8bd347965cdf1870510389331033bcf7703e71b9
Nahidfrahaman <nahi.frahaman@gmail.com>
2022-11-29 14:55:58 +0600

    mongodb basic setup is done

--- index.js
+++ index.js
@@ -8,10 +8,31 @@ app.use(express.json())
 //port 
 const port = process.env.Port ||5000
 
+//dot env require 
+require('dotenv').config()
+
 // defult api
 app.get('/', (res,reaq)=>{
     res.send('server is on')
 })
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster001.flqwhrj.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+
+
+    }
+    finally{
+
+
+        
+    }
+}
 
 //port listen
 app.listen(port, ()=>{
